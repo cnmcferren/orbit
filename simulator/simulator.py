194c26f46ab5b4a7d99872dd8c3705a7df573523
Tyler-Darby <tyler@tylerdarby.me>
2020-10-23 13:14:34 -0400

    tweaked the simulator to send more packets woooo

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -11,25 +11,25 @@ host = 'localhost'
 port = 10015
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-f = open(filename)
-contents = f.read()
 count = 0
 
 print("orbit packet simulator")
 print("sending packets over UDP every 5 seconds")
 
 while True:
-    hexData = contents[:]
-    pBytes = bytearray(98)
-    # construct a byte array of the packet 
-    for x in (range(98)):
-        temp = hexData[0:2]
-        if temp != '':
-            pBytes[x] = int(temp, 16)
-        else: 
-            pBytes[x] = 0
-        hexData = hexData[2:]
-    sock.sendto(pBytes, (host, port))
-    count += 1
-    print(str(count) + " packet(s) sent to " + host + ":" + str(port), end="\r")
-    sleep(5)
+    with open(filename) as f:
+        for packet in f:
+            packet = packet.rstrip()
+            pBytes = bytearray(98)
+            # construct a byte array of the packet 
+            for x in (range(98)):
+                temp = packet[0:2]
+                if temp != '':
+                    pBytes[x] = int(temp, 16)
+                else: 
+                    pBytes[x] = 0
+                packet = packet[2:]
+            sock.sendto(pBytes, (host, port))
+            count += 1
+            print(str(count) + " packet(s) sent to " + host + ":" + str(port), end="\r")
+            sleep(1)
